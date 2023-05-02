94f4ab2f12167f701f04bc0620d0ccb173a9df05
ethanw03 <ethanwong98@yahoo.ca>
2023-05-01 22:28:05 -0700

    create homepage

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -1,3 +1,14 @@
+import { AnimatePresence, motion } from 'framer-motion';
+import { DecalTypes, EditorTabs, FilterTabs } from '../config/constants';
+import { downloadCanvasToImage, reader } from '../config/helpers';
+import { fadeAnimation, slideAnimation } from '../config/motion';
+import { useEffect, useState } from 'react';
+
+import config from '../config/config';
+import { download } from '../assets';
+import state from '../store';
+import { useSnapshot } from 'valtio';
+
 const Customizer = () => {
   return <div>Customizer</div>;
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,63 @@
+import { AnimatePresence, motion } from 'framer-motion';
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from '../config/motion';
+
+import { CustomButton } from '../components';
+import state from '../store';
+import { useSnapshot } from 'valtio';
+
 const Home = () => {
-  return <div>Home</div>;
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section
+          className='home'
+          {...slideAnimation('left')}
+        >
+          <motion.header {...slideAnimation('down')}>
+            <img
+              src='./threejs.png'
+              alt='logo'
+              className='object-contain w-8 h-8'
+            />
+          </motion.header>
+          <motion.div
+            className='font-bold text-9xl'
+            {...headContainerAnimation}
+          >
+            <motion.div {...headTextAnimation}>
+              <h1 className='head-text'>
+                LET'S <br className='hidden xl:block' /> DO IT.
+              </h1>
+            </motion.div>
+
+            <motion.div
+              {...headContentAnimation}
+              className='flex flex-col gap-5'
+            >
+              <p className='max-w-md text-base font-normal text-grey-600'>
+                Create your unique and exclusive shirt with out brand-new 3D
+                customization tool. <strong>Unleash you imagination</strong> and
+                define your own style.
+              </p>
+              <CustomButton
+                type='filled'
+                title='Customize It'
+                handleClick={() => (state.intro = false)}
+                customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Home;
